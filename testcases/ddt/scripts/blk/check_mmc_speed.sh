9d8dda4ea7fb2c86769cd37487219d21eb874ccc
Minas Hambardzumyan <minas@ti.com>
2022-08-11 17:27:57 -0500

    am62axx-sk: added new platform support
    Signed-off-by: Minas Hambardzumyan <minas@ti.com>

--- testcases/ddt/scripts/blk/check_mmc_speed.sh
+++ testcases/ddt/scripts/blk/check_mmc_speed.sh
@@ -32,7 +32,7 @@ if [[ "$expected_mode" = "" ]]; then
         expected_mode="DDR52";;
       dra7xx-evm | dra72x-evm )
         expected_mode="HS200";;
-      am654x-evm | am654x-idk | j721e* | am62xx*)
+      am654x-evm | am654x-idk | j721e* | am62xx* | am62axx*)
         expected_mode="HS200";;
       am64xx-evm | am64xx-hsevm | j7200* | j721s* | j784*)
         expected_mode="HS400";;
--- testcases/ddt/scripts/gpio/gpio.sh
+++ testcases/ddt/scripts/gpio/gpio.sh
@@ -121,7 +121,7 @@ case $MACHINE in
   am654x-evm|am654x-idk|j721e*|j784*)
     gpio_nums="285,344,440"
   ;;
-  am64xx*|am62xx*)
+  am64xx*|am62xx*|am62axx*)
     gpio_nums="420,450,325"
   ;;
   j720*|j721s*)
