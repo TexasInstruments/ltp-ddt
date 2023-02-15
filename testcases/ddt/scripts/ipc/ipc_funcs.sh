86d199108e7360ef78010bd26989e31123c2c1f4
M V Pratap Reddy <x0257344@ti.com>
2023-02-16 03:36:07 +0530

    Updated gpio and ipc config for AM68 & AM69 SK

--- testcases/ddt/scripts/ipc/ipc_funcs.sh
+++ testcases/ddt/scripts/ipc/ipc_funcs.sh
@@ -32,7 +32,7 @@ setup_firmware()
   local __fw_dst
 
   case $MACHINE in
-      *j721*|*j7200*|*am64xx*|*am62xx*|*am65*|*j784*)
+      *j721*|*j7200*|*am64xx*|*am62xx*|*am65*|*j784*|*am68*|*am69*)
           # K3 devices don't yet support loading the firmware
           return
       ;;
@@ -105,7 +105,7 @@ rm_ipc_mods()
   local __modules=(rpmsg_rpc rpmsg_proto rpmsg_client_sample omap_remoteproc ti_k3_r5_remoteproc keystone_remoteproc remoteproc virtio_rpmsg_bus rpmsg_core)
 
   case $MACHINE in
-    *j721*|*j7200*|*am64xx*|*am62xx*|*am65*|*j784*)
+    *j721*|*j7200*|*am64xx*|*am62xx*|*am65*|*j784*|*am68*|*am69*)
 
       # K3 devices do not yet support module unloading for remote procs because the firmware cannot be reloaded
       return
@@ -139,7 +139,7 @@ ins_ipc_mods()
         modprobe ${__mod}
       done
     ;;
-    *j721*|*j7200*|*am64xx*|*am62xx*|*am65*|*j784*)
+    *j721*|*j7200*|*am64xx*|*am62xx*|*am65*|*j784*|*am68*|*am69*)
       # K3 devices do not yet support module loading for remote procs because the firmware cannot be reloaded
       return
     ;;
@@ -278,7 +278,7 @@ start_mpm_daemon()
 get_num_remote_procs()
 {
   case $SOC in
-    *j721*|*j7200*|*am64xx*|*am62xx*|*am65*|*j784*)
+    *j721*|*j7200*|*am64xx*|*am62xx*|*am65*|*j784*|*am68*|*am69*)
       # returns only those procs that have ping-pong
       echo $(cat /sys/class/remoteproc/*/name | grep -iE 'r5f|dsp|m4f' | wc -l)  
       ;;
@@ -920,7 +920,7 @@ rpmsg_client_sample_test()
   local __delay=3
   
   case $SOC in
-    *j721*|*j7200*|*am64xx*|*am62xx*|*am65*|*j784*)
+    *j721*|*j7200*|*am64xx*|*am62xx*|*am65*|*j784*|*am68*|*am69*)
         rpmsg_client_sample_test_k3 $*
         return $?
     ;;
@@ -993,7 +993,7 @@ toggle_rprocs()
   local __mbox
 
   case $MACHINE in
-      *j721*|*j7200*|*j784*)
+      *j721*|*j7200*|*j784*|*am68*|*am69*)
           # K3 devices don't yet support toggling the remote proc
           return
       ;;
@@ -1114,7 +1114,7 @@ list_pru_devs()
 list_rprocs()
 {
   case $SOC in # k3 split procs are not represented
-    j721|j784)
+    j721|j784|am68|am69)
       echo "41000000.r5f 5c00000.r5f 5e00000.r5f 4d80800000.dsp 4d81800000.dsp 64800000.dsp" 
     ;;
     j7200)
