9cd79eb87451d31082322375c838f0d0c8711857
Vardhan <x1107822@ti.com>
2023-02-28 15:57:45 +0530

    scripts:drm:alpha-blending.py, cropping.py, global-alpha-blending.py, zorder.py: Modified DRM files to add support for AM68 & AM69

--- testcases/ddt/scripts/drm/cropping.py
+++ testcases/ddt/scripts/drm/cropping.py
@@ -101,7 +101,7 @@ for i in range(len(planes)):
         "CRTC_W": p_w,
         "CRTC_H": p_h
     }
-    if re.match("am65.*|j721.*|j784.*", platform, re.I):
+    if re.match("am65.*|j721.*|j784.*|am68.*|am69.*", platform, re.I):
         p_props["zpos"] = i
     elif re.match("k2.*", platform, re.I):
         pass
