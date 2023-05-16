a9db33fac6991ad7476b44d79b21e2b9fcc93bf6
Bhavya Kapoor <b-kapoor@ti.com>
2023-05-16 15:59:53 +0530

    testcases/ddt/scripts/blk/check_standard.sh: Create Test for eMMC Standard
    Create a testcase to check the version of eMMC standard supported
    by the MMC Controller.
    
    Signed-off-by: Bhavya Kapoor <b-kapoor@ti.com>

--- testcases/ddt/scripts/blk/blk_device_common.sh
+++ testcases/ddt/scripts/blk/blk_device_common.sh
@@ -347,3 +347,19 @@ find_all_scsi_drives() {
     exit 1
   fi
 }
+
+# return: /dev/mmcblk0 etc
+find_emmc_basenode() {
+    emmc_node=`ls /dev/mmcblk* |grep boot |head -1 |sed s'/boot[0-9]*//' `
+    echo $emmc_node
+}
+
+find_mmc_basenode() {
+    emmc_node=`find_emmc_basenode` 
+    if [ -n "$emmc_node" ]; then
+      mmc_node=`ls /dev/mmcblk* |sed s",$emmc_node.*$,,g" |grep -E ".*blk[[:digit:]]+$" |head -1`
+    else
+      mmc_node=`ls /dev/mmcblk* |grep -E ".*blk[[:digit:]]+$" |head -1`
+    fi  
+    echo $mmc_node
+}
