a3de88621ca2d534051fcb95e19253f8e77e1fdf
Jai Luthra <j-luthra@ti.com>
2024-06-19 14:50:33 +0530

    ddt: scripts: setupcamera.sh: Update formats on all subdevs
    With ti-linux kernel 6.6 formats on all v4l2 subdevs in the media
    pipeline should be set, as the upstream v4l2 APIs require each subdev
    driver to validate links to the connected subdevs.
    
    Make sure we update format on cdns-csi2rx bridge and ticsi2rx shim
    subdevs for IMX219 and OV5640 in the setup script.
    
    Signed-off-by: Jai Luthra <j-luthra@ti.com>

--- testcases/ddt/scripts/common/setupcamera.sh
+++ testcases/ddt/scripts/common/setupcamera.sh
@@ -271,7 +271,11 @@ setup_imx219(){
         media-ctl -d /dev/media$media_id --set-v4l2 ''"\"imx219 $name\""':0 '$IMX219_CAM_FMT''
 
         CSI_BRIDGE_NAME=`media-ctl -d /dev/media$media_id -p -e "imx219 $name" | grep csi-bridge | cut -d "\"" -f 2`
+        media-ctl -d /dev/media$media_id --set-v4l2 ''\"$CSI_BRIDGE_NAME\"':0/0 '$IMX219_CAM_FMT''
+
         CSI2RX_NAME=`media-ctl -d /dev/media$media_id -p -e "$CSI_BRIDGE_NAME" | grep "ticsi2rx\"" | cut -d "\"" -f 2`
+        media-ctl -d /dev/media$media_id --set-v4l2 ''\"$CSI2RX_NAME\"':0/0 '$IMX219_CAM_FMT''
+
         CSI2RX_CONTEXT_NAME="$CSI2RX_NAME context 0"
 
         CAM_DEV=`media-ctl -d /dev/media$media_id -p -e "$CSI2RX_CONTEXT_NAME" | grep video | awk '{print $4}'`
@@ -296,7 +300,7 @@ setup_imx219(){
 }
 
 setup_ov5640(){
-    OV5640_CAM_FMT='[fmt:UYVY8_1X16/640x480@1/30]'
+    OV5640_CAM_FMT='[fmt:UYVY8_1X16/640x480]'
     count=0
     for media_id in {0..3}; do
     for name in `media-ctl -d /dev/media$media_id -p | grep entity | grep ov5640 | cut -d ' ' -f 5`; do
@@ -304,7 +308,11 @@ setup_ov5640(){
         media-ctl -d /dev/media$media_id --set-v4l2 ''"\"ov5640 $name\""':0 '$OV5640_CAM_FMT''
 
         CSI_BRIDGE_NAME=`media-ctl -d /dev/media$media_id -p -e "ov5640 $name" | grep csi-bridge | cut -d "\"" -f 2`
+        media-ctl -d /dev/media$media_id --set-v4l2 ''\"$CSI_BRIDGE_NAME\"':0/0 '$OV5640_CAM_FMT''
+
         CSI2RX_NAME=`media-ctl -d /dev/media$media_id -p -e "$CSI_BRIDGE_NAME" | grep "ticsi2rx\"" | cut -d "\"" -f 2`
+        media-ctl -d /dev/media$media_id --set-v4l2 ''\"$CSI2RX_NAME\"':0/0 '$OV5640_CAM_FMT''
+
         CSI2RX_CONTEXT_NAME="$CSI2RX_NAME context 0"
 
         CAM_DEV=`media-ctl -d /dev/media$media_id -p -e "$CSI2RX_CONTEXT_NAME" | grep video | awk '{print $4}'`
