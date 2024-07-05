d70274b8916806b53d878fd7ae8e73f3b2f9bcee
Aniket Limaye <a-limaye@ti.com>
2024-07-05 12:55:09 +0530

    testcases: ddt: scripts: cpuloadgen: Add script to test cpuloadgen
    Signed-off-by: Aniket Limaye <a-limaye@ti.com>

---
+++ testcases/ddt/scripts/cpuloadgen/cpuloadgen_test.sh
@@ -0,0 +1,28 @@
+#! /bin/bash
+#
+# Copyright (C) 2024 Texas Instruments Incorporated - https://www.ti.com/
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License as
+# published by the Free Software Foundation version 2.
+#
+# This program is distributed "as is" WITHOUT ANY WARRANTY of any
+# kind, whether express or implied; without even the implied warranty
+# of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# cpuloadgen_test.sh
+# Verifies whether cpuloadgen runs without errors on nproc number of cores
+# usage:  cpuloadgen_test.sh
+
+
+source "functions.sh"
+
+output=$( cpu_load_nproc 100 10 )
+if [[ $? -eq 0 ]]
+then
+		echo "Successful execution : \"$output\""
+else
+		die "Unsuccessful execution : \"$output\""
+fi
+
