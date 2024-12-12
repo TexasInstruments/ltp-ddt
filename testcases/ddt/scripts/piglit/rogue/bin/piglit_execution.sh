1ec0d4a899bf57fb1e332e2b4d71ec61cb8a0c38
Antonios Christidis <a-christidis@ti.com>
2024-12-12 15:54:14 -0600

    runtest : ddt : piglit_extensions GLES3.2 Supported Extension Piglit Tests
    Introducing piglit tests that test specific extensions, as supposed to our
    already existing piglit tests that go over specific texture formats.
    
    Changed install location of bash script responsible for executing tests,
    so that it can be part of the path. Combined piglit execute scripts for
    both extensions and texture formats.
    
    Signed-off-by: Antonios Christidis <a-christidis@ti.com>

---
+++ testcases/ddt/scripts/piglit/rogue/bin/piglit_execution.sh
@@ -0,0 +1,47 @@
+#! /bin/bash
+source "common.sh"
+
+OPTSTRING=":x:y:z:i:a:"s
+
+while getopts ${OPTSTRING} opt; do
+  case ${opt} in
+    x)
+      TESTFILE=${OPTARG};;
+    y)
+      TESTPROFILE=${OPTARG};;
+    i)
+      TEXTURE=${OPTARG};;
+    z)
+      API=${OPTARG};;
+    a)
+      ARCHITECTURE=${OPTARG};;
+    :)
+      echo "Option -${OPTARG} requires an argument."
+      exit 1
+      ;;
+    ?)
+      echo "Invalid option: -${OPTARG}."
+      exit 1
+      ;;
+  esac
+done
+
+
+if [ -z "${TEXTURE}" ]; then
+    TESTFILEPATH=/opt/ltp/testcases/ddt/scripts/piglit/${ARCHITECTURE}/extensions/${API}/${TESTFILE}
+else
+    TESTFILEPATH=/opt/ltp/testcases/ddt/scripts/piglit/${ARCHITECTURE}/testgroups/${TEXTURE}/${API}/${TESTFILE}
+fi
+
+swapfile_create
+
+piglit run ${TESTPROFILE} results/ --test-list ${TESTFILEPATH}
+
+piglit_exit=$?
+
+output=$(piglit summary console results -p)
+
+if [ $piglit_exit != 0 ] || echo "${output}" | grep -q 'failed$'
+then
+    die "${output}"
+fi
