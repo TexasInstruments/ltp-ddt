3876fc246fbe488e66bd15e19821f9c28eb0fe7d
Jayesh Choudhary <j-choudhary@ti.com>
2025-04-04 16:26:42 +0530

    alsa: performance: fix performance test cases
    Add support for performance testing considering j722s and am62p platforms,
    where DDR is not fast enough and we see UNDERRUNS due to small periodsize.
    Add a shell script to run perf testing based on testtype: play or capture.
    Also add appropriate period-size argument based on platforms.
    Then iterate the perf test command over standard sample rates.
    Data size for the input file used is 10485760 bytes which means that for
    the sampling rate of 8000, it would take ~11min. So remove that sampling
    rate as well from the list.
    
    Signed-off-by: Jayesh Choudhary <j-choudhary@ti.com>

---
+++ testcases/ddt/scripts/common/run_alsa_perf_test.sh
@@ -0,0 +1,43 @@
+source "common.sh"
+
+############################# Functions #######################################
+usage()
+{
+	cat <<-EOF >&2
+	usage: ./${0##*/}  <test type: playback/record>
+	EOF
+	exit 0
+}
+
+################################ CLI Params ####################################
+# Please use getopts
+while getopts  :h arg
+do case $arg in
+        h)      usage;;
+        :)      die "$0: Must supply an argument to -$OPTARG.";;
+        \?)     die "Invalid Option -$OPTARG ";;
+esac
+done
+
+############################ USER-DEFINED Params ##############################
+# Try to avoid defining values here, instead see if possible
+# to determine the value dynamically
+test_type=$1
+
+case $SOC in
+am6*|j722s*)
+	PERIODSIZE=256
+;;
+j784s4*|j721*)
+	PERIODSIZE=64
+;;
+esac
+
+########################### REUSABLE TEST LOGIC ###############################
+
+RATES=(11025 16000 22050 24000 32000 44100 48000 88200 96000)
+
+for i in "${RATES[@]}"
+do
+        do_cmd run_alsa_perf.sh -${test_type} 0 -rate $i -periodsize ${PERIODSIZE} -performance
+done
