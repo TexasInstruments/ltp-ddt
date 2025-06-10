e12d7b97d7392d0ea6f18d8dca3dc2fe6583114f
Antonios Christidis <a-christidis@ti.com>
2025-06-10 12:10:31 -0500

    testcases: ddt: chromium: Add support for vulkan backend
    With Chromium version 132, the sdk now supports a vulkan back-end for
    chromium. Add support for testing the new back-end.
    
    The script will not be expecting the back-end to be specified by the
    chromium test within vatf-scripts.
    
    Signed-off-by: Antonios Christidis <a-christidis@ti.com>

--- testcases/ddt/scripts/chromium/chromium_test.py
+++ testcases/ddt/scripts/chromium/chromium_test.py
@@ -20,12 +20,12 @@ def test_setup():
     subprocess.run("opkg install chromium-ozone-wayland", shell=True, check = True)
 
 
-def take_screenshots():
+def take_screenshots(backend):
     """Take screenshots utilizing weston-screenshoter"""
     os.environ['WAYLAND_DISPLAY'] = '/run/user/1000/wayland-1'
-    cmd = "su -l weston -c 'export https_proxy=http://webproxy.ext.ti.com:80; \
+    cmd = f"su -l weston -c 'export https_proxy=http://webproxy.ext.ti.com:80; \
             export XDG_RUNTIME_DIR=/run/user/1000;\
-            export WAYLAND_DISPLAY=wayland-1; chromium \"https://webglsamples.org/aquarium/aquarium.html\" --start-fullscreen --no-first-run' "
+            export WAYLAND_DISPLAY=wayland-1; chromium --use-angle={backend} \"https://webglsamples.org/aquarium/aquarium.html\" --start-fullscreen --no-first-run' "
 
     with subprocess.Popen(cmd, shell=True) as chrome:
         try:
@@ -95,11 +95,17 @@ def clean_up(png_files):
 def main():
     """Main function"""
 
+    try:
+        backend = sys.argv[1] # Specify the backend as an argument : vulkan or gles-egl (default)
+    except IndexError:
+         sys.stderr.write("Error: Backend argument is missing\n")
+         sys.exit(1)
+
     test_setup()
 
     print("Start waiting for Chromium and the benchmark itself to stabolize")
 
-    take_screenshots()
+    take_screenshots(backend)
 
     # Get list of .png pictures
     png_files = pathlib.Path(".").glob("*.png")
