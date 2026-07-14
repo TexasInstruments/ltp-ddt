06a2170f363eda680dd784f21f5ffccbab364afd
Randolph Sapp <rs@ti.com>
2026-07-14 15:38:11 -0500

    chromium_test: black format the file
    Signed-off-by: Randolph Sapp <rs@ti.com>

--- testcases/ddt/scripts/chromium/chromium_test.py
+++ testcases/ddt/scripts/chromium/chromium_test.py
@@ -1,27 +1,29 @@
-""" Required modules for test """
-import re
-import time
-import subprocess
+"""Required modules for test"""
+
 import os
-import sys
 import pathlib
+import re
+import subprocess
+import sys
+import time
+
 from PIL import Image
 import pytesseract
 
 
 def test_setup():
-    """Restart weston in debug mode and set up enviroment variables"""    
+    """Restart weston in debug mode and set up enviroment variables"""
     cmd = "sed -i 's|Exec=.*|& --debug|' /usr/share/wayland-sessions/weston.desktop"
-    subprocess.run(cmd, shell=True, check = True)
-    subprocess.run("systemctl restart emptty", shell=True, check = True)
+    subprocess.run(cmd, shell=True, check=True)
+    subprocess.run("systemctl restart emptty", shell=True, check=True)
 
     subprocess.run("opkg update", shell=True)
-    subprocess.run("opkg install chromium-ozone-wayland", shell=True, check = True)
+    subprocess.run("opkg install chromium-ozone-wayland", shell=True, check=True)
 
 
 def take_screenshots(backend):
     """Take screenshots utilizing weston-screenshoter"""
-    os.environ['WAYLAND_DISPLAY'] = '/run/user/1000/wayland-1'
+    os.environ["WAYLAND_DISPLAY"] = "/run/user/1000/wayland-1"
     cmd = f"su -l weston -c 'export https_proxy=http://webproxy.ext.ti.com:80; \
             export XDG_RUNTIME_DIR=/run/user/1000;\
             export WAYLAND_DISPLAY=wayland-1; chromium --use-angle={backend} \"https://webglsamples.org/aquarium/aquarium.html\" --start-fullscreen --no-first-run' "
@@ -36,25 +38,29 @@ def take_screenshots(backend):
 
         time.sleep(15)
         print("Taking screenshots")
-        for _ in range(0,10):
-            subprocess.run("weston-screenshooter", shell=True, check = True)
+        for _ in range(0, 10):
+            subprocess.run("weston-screenshooter", shell=True, check=True)
             time.sleep(1)
         print("Finished taking screenshots")
 
         chrome.terminate()
 
+
 def process_images(png_files):
     """Use pytesseract wrapper to find fps values from the screenshots"""
-    total_fps = 0 #Total seen fps
-    fps_not_found = 0 #Number of times fps was not found
-    fps_found = 0 #number of times the fps was found
+    # Total seen fps
+    total_fps = 0
+    # Number of times fps was not found
+    fps_not_found = 0
+    # Number of times the fps was found
+    fps_found = 0
 
     for image in png_files:
         with Image.open(image) as image:
             image = image.crop((20, 20, 100, 100))
-            text = pytesseract.image_to_string(image, config='--psm 1')
+            text = pytesseract.image_to_string(image, config="--psm 1")
             try:
-                fps_value = int(re.search(r'fps:\s(\d+)', text).group(1))
+                fps_value = int(re.search(r"fps:\s(\d+)", text).group(1))
             except (AttributeError, TypeError):
                 print("No FPS value found")
                 fps_not_found += 1
@@ -74,16 +80,21 @@ def process_images(png_files):
 
     get_test_execution_result(fps_not_found)
 
+
 def get_test_execution_result(fps_not_found):
     """See if the test results are reliable or not and clean up"""
-    
+
     png_files = pathlib.Path(".").glob("*.png")
     clean_up(png_files)
-    
-    if fps_not_found > 2:   #Test result to unreliable,
-                            #fail in order notify team team something needs to be checked manually
-        print("Test execution failure, unreliable results. Too many fps values not found")
+
+    # Test result to unreliable fail in order notify team team something needs to be checked
+    # manually
+    if fps_not_found > 2:
+        print(
+            "Test execution failure, unreliable results. Too many fps values not found"
+        )
         sys.exit(1)
+
 
 def clean_up(png_files):
     """Delete the .png screenshots"""
@@ -91,11 +102,13 @@ def clean_up(png_files):
     for file in png_files:
         file.unlink()
 
+
 def main():
     """Main function"""
 
     try:
-        backend = sys.argv[1] # Specify the backend as an argument : vulkan or gles-egl (default)
+        # Specify the backend as an argument : vulkan or gles-egl (default)
+        backend = sys.argv[1]
     except IndexError:
         sys.stderr.write("Error: Backend argument is missing\n")
         sys.exit(1)
@@ -112,5 +125,5 @@ def main():
     process_images(png_files)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
